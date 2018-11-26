877495fb5d745c9460bcd154e593f755837aafd6
men7627 <men1219@hanmail.net>
2018-11-26 14:32:32 +0900

    전체 가사 출력 후 현재 재생 가사의 배경 색상 변경 버젼  1. 일반 가사 출력 완료  2. 파트 있는 가사 출력 완료  3. J-POP, POP SONG 등 한 시간 대의 여러 가사 출력 완료 ( 한 시간 대에 가사 2,3,4,5, ... 모두 가능)  4. 2/3 혼합 된 버젼도 출력 가능  5. 주석 완료

--- Presto.SWCamp.Lyrics/LyricsWindow.xaml.cs
+++ Presto.SWCamp.Lyrics/LyricsWindow.xaml.cs
@@ -21,13 +21,12 @@ namespace Presto.SWCamp.Lyrics
 {
     public partial class LyricsWindow : Window
     {
-        TextBox[] tb;
+        TextBox[] tb;                            //가사 한줄 한줄의 배열
         List<Tuple<TimeSpan, string>> splitData; //파싱 데이터 
         public LyricsWindow()
         {
             InitializeComponent();
-            //스트림이 바뀌었을 때의 이벤트 함수
-            PrestoSDK.PrestoService.Player.StreamChanged += Player_StreamChanged;
+            PrestoSDK.PrestoService.Player.StreamChanged += Player_StreamChanged;  //스트림이 바뀌었을 때의 이벤트 함수
         }
 
         private void Player_StreamChanged(object sender, Common.StreamChangedEventArgs e)
@@ -35,8 +34,8 @@ namespace Presto.SWCamp.Lyrics
 
             //가사 초기화
             //textLyrics.Text = null;
-            LyricPanel.Children.Clear();
-            splitData = new List<Tuple<TimeSpan, string>>();
+            LyricPanel.Children.Clear(); //패널 초기화
+            splitData = new List<Tuple<TimeSpan, string>>(); //파싱 데이터 리스트 초기화
 
             //lrc파일 경로 변경
             var fileName = PrestoSDK.PrestoService.Player.CurrentMusic.Path;
@@ -45,23 +44,23 @@ namespace Presto.SWCamp.Lyrics
             var lines = File.ReadAllLines(path);
             //가사 데이터 읽어 오기
             string singer = null;                    //파트 저장할 변수 
-            tb = new TextBox[lines.Length];
+            tb = new TextBox[lines.Length];          //가사 한줄 한줄의 배열 생성
             for (int i = 3; i < lines.Length; i++)
             {
-                tb[i] = new TextBox();
-                tb[i].BorderBrush = Brushes.White;
+                tb[i] = new TextBox();               //가사 한줄 생성
+                tb[i].BorderBrush = Brushes.White;   //가사 한줄의 테두리 흰색 설정
 
-                string[] data = lines[i].Split(']');
+                string[] data = lines[i].Split(']'); //가사 한줄을 ']' 단위로 스플릿
                 if (data.Length == 2)                //파트 지정이 없는 부분
                 {
                     data[1] = singer + data[1] + '\n';      //이전 파트를 붙임
                     //textLyrics.Text += data[1];
-                    tb[i].Text = data[1];
-                    LyricPanel.Children.Add(tb[i]);
+                    tb[i].Text = data[1];                   //가사 한줄(텍스트 박스)에 가사 데이터 지정
+                    LyricPanel.Children.Add(tb[i]);         //패널의 자식으로 가사 한줄 (텍스트 박스)를 지정
                 }
                 else if (data.Length == 3)           //파트 지정이 있는 부분
                 {
-                    singer = '(' + data[1].Substring(1).Trim() + ") ";
+                    singer = '(' + data[1].Substring(1).Trim() + ") "; 
                     data[1] = singer + data[2] + '\n';
                     tb[i].Text = data[1];
                     LyricPanel.Children.Add(tb[i]);
@@ -83,7 +82,7 @@ namespace Presto.SWCamp.Lyrics
         {
             for (int i = 3; i < tb.Length; i++)//모든 가사 배경 흰색
             {
-                tb[i].Background = Brushes.White;
+                tb[i].Background = Brushes.White;  
             }
             //가사 변경 범위 조정
             for (int i = 0; i < splitData.Count(); i++)
@@ -97,11 +96,11 @@ namespace Presto.SWCamp.Lyrics
                 else if (cur >= splitData[splitData.Count() - 1].Item1) //마지막 가사 출력 (인덱스 오류 방지)
                 {
                     //textLyrics.Text = splitData[splitData.Count() - 1].Item2;
-                    for (int j = 0; j < splitData.Count() - 1; j++)
+                    for (int j = 0; j <= splitData.Count() - 1; j++)
                     {
-                        if (splitData[j].Item1 == splitData[i].Item1)
+                        if (splitData[j].Item1 == splitData[splitData.Count() - 1].Item1) //마지막 가사의 시간 대의 모든 가사
                         {
-                            tb[splitData.Count() + 2].Background = Brushes.Green;
+                            tb[j+3].Background = Brushes.Green;
                         }
                     }
                     
@@ -111,9 +110,9 @@ namespace Presto.SWCamp.Lyrics
                     if (splitData[i].Item2 != null )
                     {
                         //textLyrics.Text = splitData[i].Item2;
-                        for(int j = 0; j < splitData.Count() - 1; j++)
+                        for(int j = 0; j <= splitData.Count() - 1; j++) 
                         {
-                            if (splitData[j].Item1 == splitData[i].Item1)
+                            if (splitData[j].Item1 == splitData[i].Item1) //현재 시간 대의 모든 가사
                             {
                                 tb[j + 3].Background = Brushes.Green;
                             }
